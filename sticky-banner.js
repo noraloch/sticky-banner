4f7ab324f1efd946b877860b293f9c0a9b7f571c
Noura <contact.nouraloudani@gmail.com>
2021-06-17 21:49:53 -0400

    hiding and replace features are done

--- sticky-banner.js
+++ sticky-banner.js
@@ -1,8 +1,28 @@
 const customBlockIsVisibleEvent = "block";
 const customBlockIsNotVisibleEvent = "show";
-let blockerCounter = 0;
-/* blocker visibility check helper: Using the Intersection Obeserver API. Browsers came out with not a long time ago 
-to solve the performance issues that a traditional scroll event listener has */
+
+// Store reference to current top and bottom banners
+let botBannElement;
+let topBannElement;
+
+// check if top banner is active
+const topBannerExists = () => {
+    if (document.querySelector('.top-banner')) {
+        topBannElement = document.querySelector('.top-banner');
+        return true;
+    }
+    return false
+}
+// check if bottom banner is active
+const bottomBannerExists = () => {
+    if (document.querySelector('.bottom-banner')) {
+        botBannElement = document.querySelector('.bottom-banner');
+        return true;
+    }
+    return false
+}
+
+/////////////////// Class //////////////////
 class StickyBanner {
     constructor(html, { position = 'top', hideEnabled = false }) {
         this.html = html;
@@ -11,20 +31,17 @@ class StickyBanner {
         this.renderBanner();
         if (hideEnabled) {
             document.addEventListener(customBlockIsVisibleEvent, () => {
-                this.hideBanner();
+                this.showBanner("none");
             });
             document.addEventListener(customBlockIsNotVisibleEvent, () => {
-                this.showBanner();
+                this.showBanner("inline-block");
             });
         };
     }
 
     // position check helper
     checkTopPosition() {
-        if (this.config.position === 'top') {
-            return true;
-        }
-        return false;
+        return this.config.position === 'top';
     }
 
     /* removed the element from the DOM. The instance object itself will be deleted
@@ -36,35 +53,43 @@ class StickyBanner {
 
     //render banner
     renderBanner() {
-        // check if there is a sticky banner of the same position and destroy
         let bannerElement = document.createElement("div");
         this.bannerHtmlElm = bannerElement;
-        let main = document.querySelector('main');
 
         bannerElement.innerHTML = this.html;
 
         //for style => check position
         if (this.checkTopPosition()) {
+            if (topBannerExists()) {
+                topBannElement.remove();
+            }
+
             let img = bannerElement.querySelector('img');
 
+            // Normally we would want to randomize or make sure this doesn't overlap with an existing classname on the page
+            // to prevent mis-querying and getting elements that aren't our elements.
             bannerElement.className = "top-banner";
             bannerElement.style.position = "fixed";
-            bannerElement.style.display = "inline-block"
             bannerElement.style.width = "320px";
             bannerElement.style.height = "50px";
             bannerElement.style.right = "0";
-            bannerElement.style.top = "0";
             bannerElement.style.left = "0";
+            bannerElement.style.top = "0";
 
             img.style.width = "320px"
             img.style.height = "50px"
 
         } else {
+
+            if (bottomBannerExists()) {
+                botBannElement.remove();
+            }
+            let main = document.querySelector('main');
             main.style.marginBottom = "55px"
 
             let img = bannerElement.querySelector('img');
-            bannerElement.className = "bottom-banner";
 
+            bannerElement.className = "bottom-banner";
             bannerElement.style.position = "fixed";
             bannerElement.style.width = "320px";
             bannerElement.style.height = "50px";
@@ -77,63 +102,45 @@ class StickyBanner {
         }
         document.body.appendChild(bannerElement);
     }
+
     // hide banner when block appears; 
     // css display property removes the whole element. Unlike visibility that keeps the space of the element on the document.
-    hideBanner() {
-        this.bannerHtmlElm.style.display = "none";
-    }
-    // show banner when block disappear
-    showBanner() {
-        this.bannerHtmlElm.style.display = "inline-block";
-    }
-}
 
-// check if top banner is active
-const topBannerExists = () => {
-    if (document.querySelector('.top-banner')) {
-        return true;
+    // change display of banner
+    showBanner(display) {
+        this.bannerHtmlElm.style.display = display;
     }
-    return false
 }
-// check if bottom banner is active
-const bottomBannerExists = () => {
-    if (document.querySelector('.bottom-banner')) {
-        return true;
-    }
-    return false
-}
+//////////////////// end of class //////////////////////       
+
+/* blocker visibility check helper: Using the Intersection Obeserver API. Browsers came out with it not a long time ago 
+to solve the performance issues that a traditional scroll event listener has */
+
+// Normally, Check for exsistence of IntersectionObserver (and other APIs) before we try and use them. 
+// Task said to assume we are using a modern browser.
 window.addEventListener('DOMContentLoaded', (event) => {
+    const outOfViewEvent = new Event(customBlockIsNotVisibleEvent);
+    const inViewEvent = new Event(customBlockIsVisibleEvent);
+
     let blockElements = document.querySelectorAll('.blocker');
-    // console.log(blockElements)
-    let observer = new IntersectionObserver(function (entries) {
-        // isIntersecting is true when element and viewport are overlapping
-        // isIntersecting is false when element and viewport don't overlap
-        if (entries[0].isIntersecting) {
-            console.log('blocker is visible');
-            blockerCounter++;
-            console.log(blockerCounter)
-            const inViewEvent = new Event(customBlockIsVisibleEvent);
-            document.dispatchEvent(inViewEvent);
-        } else if (!entries[0].isIntersecting) {
-            console.log('blocker is not visible');
-            decrement()
-            // console.log('dec', blockerCounter)
-            if (blockerCounter === 0) {
-                const outOfViewEvent = new Event(customBlockIsNotVisibleEvent);
-                document.dispatchEvent(outOfViewEvent);
+
+    // Keep track of the visibility of each blocker individually, each blocker is kept track of by corresponding index in this array.
+    // 1 - indicates that the blocker at that index is visible, 0 - indicates that that blocker is not visible
+    let tracker = [];
+    blockElements.forEach((blockElement, i) => {
+        let observer = new IntersectionObserver(function (entries) {
+            let entry = entries[0];
+            if (entry.isIntersecting) {
+                tracker[i] = 1;
+            } else if (!entry.isIntersecting) {
+                tracker[i] = 0;
             }
-
-        }
-    }, { threshold: [0] });
-
-    blockElements.forEach(blockElement => {
+            if (tracker.every(e => e === 0)) {
+                document.dispatchEvent(outOfViewEvent);
+            } else {
+                document.dispatchEvent(inViewEvent);
+            }
+        }, { threshold: [0] });
         observer.observe(blockElement);
     })
 });
-
-//decrement function
-function decrement() {
-    if (blockerCounter > 0) {
-        blockerCounter = blockerCounter - 1;
-    }
-}
